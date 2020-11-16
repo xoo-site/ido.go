5486ca145d6725a2226d2f40d181aae921dddb63
Jeeyshe <jeeyshe@gmail.com>
2020-11-16 10:08:00 +0800

    hash

---
+++ hash/hash.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	md52 "crypto/md5"
+)
+
+type LogAlert struct {
+	Path    string
+	context string
+	count   int
+}
+
+func main() {
+	md5 := md52.New()
+	write, _ := md5.Write([]byte("Nhacccccc"))
+	print(write)
+	print(string(md5.Sum([]byte("NIhay"))))
+	fmt.Printf("%x\n", md5.Sum([]byte("Nhacccccddssgggdhhhhhhhsssssshhhhhhhhhh")))
+	fmt.Printf("%x\n", md5.Sum([]byte("123")))
+	fmt.Printf("%x\n", md5.Sum([]byte("1234")))
+	fmt.Printf("%x\n", md5.Sum([]byte("12345")))
+	fmt.Printf("%x\n", md5.Sum([]byte("1234")))
+	fmt.Printf("%x\n", md5.Sum([]byte("1234")))
+}
